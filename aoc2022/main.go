b1874cbf0807d6e48c6deb603407b7d2a6893108
Marvin Becker <marvinbecker@mail.de>
2023-11-19 19:04:03 +0100

    Add invocation for day 16 to main function

--- aoc2022/main.go
+++ aoc2022/main.go
@@ -2,6 +2,7 @@ package aoc2022
 
 import (
 	aoc2022d1 "aoc.mb/aoc2022/1"
+	aoc2022d16 "aoc.mb/aoc2022/16"
 	aoc2022d21 "aoc.mb/aoc2022/21"
 	aoc2022d8 "aoc.mb/aoc2022/8"
 )
@@ -13,6 +14,8 @@ func Aoc(day string, level int, input []byte) int {
 		ret = aoc2022d1.Run(input, level)
 	case "8":
 		ret = aoc2022d8.Run(input, level)
+	case "16":
+		ret = aoc2022d16.Run(input, level)
 	case "21":
 		ret = aoc2022d21.Run(input, level)
 	}
